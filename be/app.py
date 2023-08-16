88813744cdb5943399552341e32641659beff2af
JacquesJ44 <jacquesj44@gmail.com>
2023-08-16 08:45:31 +0200

    version 1.0

--- be/app.py
+++ be/app.py
@@ -1,48 +1,57 @@
 import routeros_api
+import contextlib
+import io
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 from datetime import datetime
-from basic_config import address_list, fwrules, admin_user_pw, new_users, disabled, allowed_interfaces, name, security_profile, wlan1_config, wlan2_config, sntp
+from basic_config import address_list, fwrules, admin_user_pw, new_users, disabled, allowed_interfaces, security_profile, wlan1_config, wlan2_config, sntp
 
 
 app = Flask(__name__)
 
 # Connect to router
-connection = routeros_api.RouterOsApiPool(
-    host='192.168.88.1',
-    username='admin',
-    password='',
-    port=None,
-    # use_ssl=True,
-    ssl_verify=False,
-    ssl_verify_hostname=False,
-    # ssl_context=False,
-    plaintext_login=True
-)
+def connect_router():
+    connection = routeros_api.RouterOsApiPool(
+        host='192.168.88.1',
+        username='admin',
+        password='',
+        port=None,
+        # use_ssl=True,
+        ssl_verify=False,
+        ssl_verify_hostname=False,
+        # ssl_context=False,
+        plaintext_login=True
+    )
 
-api = connection.get_api()
+    # print(connection)
+    return connection
 
-# def connection():
-#     # print('Connected!')
-#     # print('\n')
-#     return {'Connected'}
+
 
 @app.route('/', methods=['GET', 'POST'])
 @cross_origin(methods=['GET', 'POST'], headers=['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'], supports_credentials=True, origins='http://localhost:3000')
 def main():
    
-
-    if request.method == 'GET':
-        print('GET method')
-        return {}
+    # if request.method == 'GET':
+    #     print('GET method')
+    #     return {}
 
     if request.method == 'POST':
-        print('POST method')
+       
         obj = request.get_json()
         print(obj)
         # input()
+        captured_output = io.StringIO()
+        # Connect to router
+        try:
+            api = connect_router().get_api()
+        except:
+            return jsonify({"error": "Unable to connect!"})
+        
+        with contextlib.redirect_stdout(captured_output):
+            print("Connected!" + '\n')
         
         # First, let's set the time
         now = datetime.now()
@@ -51,88 +60,99 @@ def main():
         time1 = now.time()
         time1 = time1.strftime('%H:%M:%S')
         clock.set(date=today, time=time1)
+
         y = clock.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print(y)
+            print('\n')
 
         # STEP ONE - create address list
         addresses =  api.get_resource('/ip/firewall/address-list')
         for i in address_list:
             addresses.add(address=i['address'], comment=i['comment'], list=i['list'])
 
-        print('ADDRESS LIST ADDED')
         y = addresses.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('ADDRESS LIST ADDED')
+            print(y)
+            print('\n')
 
         # STEP TWO - create firewall rule using the address list
         fwadd = api.get_resource('/ip/firewall/filter')
         for i in fwrules:
             fwadd.add(chain=i['chain'], action=i['action'], src_address_list=i['src-address-list'], log=i['log'], log_prefix=i['log-prefix'], place_before=i['place-before'], comment=i['comment'])
 
-        print('FIREWALL RULES(S) ADDED')
         y = fwadd.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('FIREWALL RULE(S) ADDED')
+            print(y)
+            print('\n')
 
         # STEP THREE - add users and change default passwords
-        # users = api.get_resource('/user')
-        # users.set(id="*1", password=admin_user_pw)
-        # for i in new_users:
-        #     users.add(name=i['name'], password=i['password'], group=i['group'])
+        users = api.get_resource('/user')
+        users.set(id="*1", password=admin_user_pw)
+        for i in new_users:
+            users.add(name=i['name'], password=i['password'], group=i['group'])
 
-        # print('USER(S) ADDED')
-        # y = users.get()
-        # print(y)
-        # print('\n')
+        y = users.get()
+        with contextlib.redirect_stdout(captured_output):
+            print('USER(S) ADDED')
+            print(y)
+            print('\n')
 
         # STEP FOUR - set MAC Telnet and MAC winbox service interfaces
         mac_telnet = api.get_resource('/tool/mac-server')
         mac_telnet.set(allowed_interface_list=allowed_interfaces)
         y = mac_telnet.get()
-        print('MAC TELNET ENABLED')
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('MAC TELNET ENABLED')
+            print(y)
+            print('\n')
         mac_winbox = api.get_resource('/tool/mac-server/mac-winbox')
         mac_winbox.set(allowed_interface_list=allowed_interfaces)
         y = mac_winbox.get()
-        print('MAC WINBOX ENABLED')
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('MAC WINBOX ENABLED')
+            print(y)
+            print('\n')
 
         # Step FIVE - set neighbor discovery interface list
         neighbors = api.get_resource('/ip/neighbor/discovery-settings')
         neighbors.set(discover_interface_list=allowed_interfaces)
         y = neighbors.get()
-        print('NEIGHBOR DISCOVERY SETTING ENABLED')
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('NEIGHBOR DISCOVERY SETTING ENABLED')
+            print(y)
+            print('\n')
 
         # Step SIX - set identity
         router_id = api.get_resource('/system/identity')
-        # a = qlist.get()
-        # print(a)
         router_id.set(name=obj['router_id'])
-        print('ROUTER NAME')
+
         y = router_id.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('ROUTER NAME')
+            print(y)
+            print('\n')
 
         # Step SIX(b) - set up wireless security profile and wireless SSID
         # Setup up 'Basic-Security' Profile
         basic_sec = api.get_resource('/interface/wireless/security-profiles')
-        basic_sec.add(authentication_types=security_profile['authentication-types'], mode=security_profile['mode'], name=security_profile['name'], supplicant_identity=security_profile['supplicant-identity'], wpa_pre_shared_key=obj['ssid2_pw'], wpa2_pre_shared_key=obj['ssid2_pw'])
-        print('SECURTIY PROFILE')
+        basic_sec.add(authentication_types=security_profile['authentication-types'], mode=security_profile['mode'], name=security_profile['name'], supplicant_identity=security_profile['supplicant-identity'], wpa_pre_shared_key=obj['wpa_pass'], wpa2_pre_shared_key=obj['wpa2_pass'])
+
         y = basic_sec.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('SECURTIY PROFILE')
+            print(y)
+            print('\n')
 
         # Configure wireless profiles
         wireless_wlan = api.get_resource('/interface/wireless')
 
         wlan1 = wireless_wlan.get(default_name='wlan1')
         if wlan1:
-            print('wlan1 found - configuring')
+            with contextlib.redirect_stdout(captured_output):
+                print('wlan1 found - configuring')
             idwlan1 = wlan1[0]['id']
             wireless_wlan.set(id=idwlan1,
             band=wlan1_config['band'],
@@ -144,21 +164,24 @@ def main():
             installation=wlan1_config['installation'], 
             mode=wlan1_config['mode'],
             security_profile=wlan1_config['security-profile'], 
-            ssid=obj['ssid2_un'], 
+            ssid=obj['ssid24'], 
             wireless_protocol=wlan1_config['wireless-protocol']
             )
         else:
-            print('no wlan1 found')
-            print('\n')
+            with contextlib.redirect_stdout(captured_output):
+                print('no wlan1 found')
+                print('\n')
 
         wlan1 = wireless_wlan.get(default_name='wlan1')
-        print('WLAN1 CONFIG')
-        print(wlan1)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('WLAN1 CONFIG')
+            print(wlan1)
+            print('\n')
 
         wlan2 = wireless_wlan.get(default_name='wlan2')
         if wlan2:
-            print('wlan2 found - configuring')
+            with contextlib.redirect_stdout(captured_output):
+                print('wlan2 found - configuring')
             idwlan2 = wlan2[0]['id']
             wireless_wlan.set(id=idwlan2,
             band=wlan2_config['band'],
@@ -170,25 +193,29 @@ def main():
             installation=wlan2_config['installation'], 
             mode=wlan2_config['mode'],
             security_profile=wlan2_config['security-profile'], 
-            ssid=obj['ssid5_un'], 
+            ssid=obj['ssid58'], 
             wireless_protocol=wlan2_config['wireless-protocol']
             )
         else:
-            print('no wlan2 found')
-            print('\n')
+            with contextlib.redirect_stdout(captured_output):
+                print('no wlan2 found')
+                print('\n')
 
         wlan2 = wireless_wlan.get(default_name='wlan2')
-        print('WLAN2 CONFIG')
-        print(wlan2)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('WLAN2 CONFIG')
+            print(wlan2)
+            print('\n')
 
         # Step SEVEN - set SNTP Client
         ntp = api.get_resource('/system/ntp/client')
         ntp.set(enabled=sntp['enabled'], server_dns_names=sntp['server-dns-names'])
-        print('SET SNTP CLIENT')
+
         y = ntp.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('SET SNTP CLIENT')
+            print(y)
+            print('\n')
 
         # Step EIGHT - disable services
         ip_services = api.get_resource('/ip/service')
@@ -197,22 +224,21 @@ def main():
         for i in disabled:
             ip_services.set(id=i['id'], disabled=i['disabled'])
 
-        print('SERVICES DISABLED')
         y = ip_services.get()
-        print(y)
-        print('\n')
+        with contextlib.redirect_stdout(captured_output):
+            print('SERVICES DISABLED')
+            print(y)
+            print('\n')
 
-        print('\n')
-        print('Router configured successfully!')
-    else:
-        print('No router connected')
+        connect_router().disconnect()
+        with contextlib.redirect_stdout(captured_output):
+            print('Router configured successfully!')
+            print('Disconnected - you will not be able to reconnect. Use Winbox from here onwards...')
+            print('\n')
 
-    connection.disconnect()
-    print('Disconnected - you will not be able to reconnect. Use Winbox from here onwards...')
-    print('\n')
+        captured_string = captured_output.getvalue()
 
-    return jsonify({"msg": 'complete'})
-
+        return jsonify({"msg": "Successful"}, captured_string) 
 
 if __name__ == "__main__":
     CORS(app, supports_credentials=True, resource={r"/*": {"origins": "*"}})
--- fe/hermoor/src/App.js
+++ fe/hermoor/src/App.js
@@ -1,26 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { IP } from './config.js';
 
-import logo from './logo.svg';
+import AesirLogo from './AesirLogo.png';
 import './App.css';
 import './index.css';
 
 function App() {
   
   const [router_id, setRouter_id] = useState('')
-  const [ssid2_un, setSsid2_un] = useState('')
-  const [ssid2_pw, setSsid2_pw] = useState('')
-  const [ssid5_un, setSsid5_un] = useState('')
-  const [ssid5_pw, setSsid5_pw] = useState('')
+  const [ssid24, setSsid24] = useState('')
+  const [ssid58, setSsid58] = useState('')
+  const [wpa_pass, setWpa_pass] = useState('')
+  const [wpa2_pass, setWpa2_pass] = useState('')
+
+  const [output, setOutput] = useState('')
+
+  function handleChange(evt) {
+    const value = evt.target.value;
+    setOutput(value);
+  }
+  function handleReset() {
+    const value = '';
+    setOutput(value);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = {
       router_id: router_id,
-      ssid2_un: ssid2_un,
-      ssid2_pw: ssid2_pw,
-      ssid5_un: ssid5_un,
-      ssid5_pw: ssid5_pw
+      ssid24: ssid24,
+      ssid58: ssid58,
+      wpa_pass: wpa_pass,
+      wpa2_pass: wpa2_pass
     };
     // console.log(form)
     fetch(IP + '/', {
@@ -34,75 +45,107 @@ function App() {
     }).then(res => {
       console.log(res);
       return res.json();
-  }) .then(data => {
-    console.log(data)
+    }).then(data => {
+      console.log(data)
+      if ('error' in data) {
+        alert(data['error'])
+      } else {
+        setOutput(data[1]);
+        setRouter_id('');
+        setSsid24('');
+        setSsid58('');
+        setWpa_pass('');
+        setWpa2_pass('');
+      }
     })
 }
-// .then(() => {
-//     navigate('/antrag/3')
-//   }) 
 
   return (
-  <>  
-    <form onSubmit={handleSubmit}>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">Router ID</span>
-        </label>
-        <input placeholder="Name of Router" className="input input-bordered w-full max-w-xs"
-          type="text"
-          // required
-          value={ router_id }
-          onChange={(e) => setRouter_id(e.target.value)} 
-          />
+  <>
+    <div className="App-header">
+      <div className="navbar shadow-2xl bg-base-200 roundedborders">
+        <div className="flex-1">
+          {/* <Link to="/home"> */}
+            <img src={ AesirLogo } className="App-logo" alt="logo" />
+            <h1 className='ml-5'>hermóðr</h1>
+          {/* </Link> */}
+        </div>
       </div>
-      <div className="form-control">
-          <label className="label">
-            <span className="label-text">2.4GHz SSID</span>
-          </label>
-          <input placeholder="2.4GHz SSID" className="input input-bordered w-full max-w-xs"
-            type="text"
-            // required
-            value={ ssid2_un }
-            onChange={(e) => setSsid2_un(e.target.value)} 
-            />
-      </div>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">2.4GHz Password</span>
-        </label>
-        <input placeholder="2.4GHz Password" className="input input-bordered w-full max-w-xs"
-          type="text"
-          // required
-          value={ ssid2_pw }
-          onChange={(e) => setSsid2_pw(e.target.value)} 
-          />
-      </div>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">5GHz SSID</span>
-        </label>
-        <input placeholder="5GHz SSID" className="input input-bordered w-full max-w-xs"
-          type="text"
-          // required
-          value={ ssid5_un }
-          onChange={(e) => setSsid5_un(e.target.value)} 
-          />
-      </div>
-      <div className="form-control">
-        <label className="label">
-          <span className="label-text">5GHz Password</span>
-        </label>
-        <input placeholder="5GHz Password" className="input input-bordered w-full max-w-xs"
-          type="text"
-          // required
-          value={ ssid5_pw }
-          onChange={(e) => setSsid5_pw(e.target.value)} 
-          />
-      </div>
+    </div>
 
-      <button className="btn btn-primary my-4">DO IT</button>
-    </form>
+    <div className="flow-root">
+      <div className="float-left">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control ml-5">
+            <label className="label">
+              <span className="label-text">Router ID</span>
+            </label>
+            <input placeholder="Name of Router" className="input input-bordered w-full max-w-xs"
+              type="text"
+              required
+              value={ router_id }
+              onChange={(e) => setRouter_id(e.target.value)} 
+              />
+          </div>
+          <div className="form-control ml-5">
+              <label className="label">
+                <span className="label-text">2.4GHz SSID</span>
+              </label>
+              <input placeholder="2.4GHz SSID" className="input input-bordered w-full max-w-xs"
+                type="text"
+                // required
+                value={ ssid24 }
+                onChange={(e) => setSsid24(e.target.value)} 
+                />
+          </div>
+          <div className="form-control ml-5">
+            <label className="label">
+              <span className="label-text">5.8GHz SSID</span>
+            </label>
+            <input placeholder="5.8GHz SSID" className="input input-bordered w-full max-w-xs"
+              type="text"
+              // required
+              value={ ssid58 }
+              onChange={(e) => setSsid58(e.target.value)} 
+              />
+          </div>
+          <div className="form-control ml-5">
+            <label className="label">
+              <span className="label-text">WPA Preshared Key</span>
+            </label>
+            <input placeholder="WPA Preshared Key" className="input input-bordered w-full max-w-xs"
+              type="text"
+              // required
+              pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+              value={ wpa_pass }
+              onChange={(e) => setWpa_pass(e.target.value)} 
+              />
+          </div>
+          <div className="form-control ml-5">
+            <label className="label">
+              <span className="label-text">WPA2 Preshared Key</span>
+            </label>
+            <input placeholder="WPA2 Preshared Key" className="input input-bordered w-full max-w-xs"
+              type="text"
+              // required
+              pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"            
+              value={ wpa2_pass }
+              onChange={(e) => setWpa2_pass(e.target.value)} 
+              />
+          </div>
+
+          <button className="btn btn-primary my-4 ml-5">DO IT</button>
+        </form>
+        </div>
+        
+        <div className="float-right border m-5">
+          <textarea name="outputtext" cols="150" rows="20" value={output} onChange={handleChange}></textarea>
+        </div>
+    </div>
+
+    <div className='float-right mx-5'>
+      <button className="btn btn-primary my-4 ml-5" onClick={handleReset}>Clear</button>
+    </div>
   </>
   );
 
